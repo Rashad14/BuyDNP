0a04ff761b5c822c47f28de02b9878b5b3d26592
rashad14 <rashad.seyfullayev14@gmail.com>
2023-03-09 13:52:56 +0400

    registration added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,92 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('layouts.app')
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+@section('css')
+    <style>
+        .error{
+            color: red;
+            font-size: 12px;
+        }
+    </style>
+@endsection
+
+@section('content')
+
+    <!-- BREADCRUMB AREA START -->
+    <div class="ltn__breadcrumb-area text-left bg-overlay-white-30 bg-image "  data-bs-bg="{{asset('panel/img/bg/14.jpg')}}">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="ltn__breadcrumb-inner">
+                        <h1 class="page-title">Account</h1>
+                        <div class="ltn__breadcrumb-list">
+                            <ul>
+                                <li><a href="index.html"><span class="ltn__secondary-color"><i class="fas fa-home"></i></span> Home</a></li>
+                                <li>Register</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
+    <!-- BREADCRUMB AREA END -->
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <!-- LOGIN AREA START (Register) -->
+    <div class="ltn__login-area pb-110">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title-area text-center">
+                        <h1 class="section-title">Register <br>Your Account</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br>
+                            Sit aliquid,  Non distinctio vel iste.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="account-login-inner">
+                        <form class="ltn__form-box contact-form-box" method="POST" action="{{ route('register') }}">
+                            @csrf
+
+                            <small class="error">{!! implode('', $errors->get('name')) !!}</small>
+                            <input type="text" name="name" value="{{old('name')}}" placeholder="Name*" required autofocus autocomplete="name" />
+                            <small class="error">{!! implode('', $errors->get('email')) !!}</small>
+                            <input type="email" name="email" value="{{old('email')}}" placeholder="Email*" required autocomplete="username" />
+                            <small class="error">{!! implode('', $errors->get('password')) !!}</small>
+                            <input        type="password"
+                                          name="password"
+                                          placeholder="Password*"
+                                          required autocomplete="new-password" />
+
+                            <small class="error">{!! implode('', $errors->get('password_confirmation')) !!}</small>
+                            <input        type="password"
+                                          placeholder="Password Confirmation*"
+                                          name="password_confirmation" required autocomplete="new-password" />
+
+                            <label class="checkbox-inline">
+                                <input type="checkbox" value="">
+                                I consent to Herboil processing my personal data in order to send personalized marketing material in accordance with the consent form and the privacy policy.
+                            </label>
+                            <label class="checkbox-inline">
+                                <input type="checkbox" value="">
+                                By clicking "create account", I consent to the privacy policy.
+                            </label>
+                            <div class="btn-wrapper">
+                                <button class="theme-btn-1 btn reverse-color btn-block" type="submit">CREATE ACCOUNT</button>
+                            </div>
+                        </form>
+                        <div class="by-agree text-center">
+                            <p>By creating an account, you agree to our:</p>
+                            <p><a href="#">TERMS OF CONDITIONS  &nbsp; &nbsp; | &nbsp; &nbsp;  PRIVACY POLICY</a></p>
+                            <div class="go-to-btn mt-50">
+                                <a href="login.html">ALREADY HAVE AN ACCOUNT ?</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+    <!-- LOGIN AREA END -->
+@endsection
