5a569b072d5e2c9882793c620e75425e098f9f17
rashad14 <rashad.seyfullayev14@gmail.com>
2023-03-16 10:35:47 +0400

    home slider area updated

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -2,85 +2,7 @@
 
 @section('content')
     <!-- SLIDER AREA START (slider-3) -->
-    <div class="ltn__slider-area ltn__slider-3---  section-bg-1--- mt-30">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-7">
-                    <div class="ltn__slide-active-2 slick-slide-arrow-1 slick-slide-dots-1 mb-30">
-                        <!-- ltn__slide-item -->
-                        <div class="ltn__slide-item ltn__slide-item-10 section-bg-1 bg-image" data-bs-bg="{{asset('panel/img/slider/71.jpg')}}">
-                            <div class="ltn__slide-item-inner">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-lg-7 col-md-7 col-sm-7 align-self-center">
-                                            <div class="slide-item-info">
-                                                <div class="slide-item-info-inner ltn__slide-animation">
-                                                    <h6 class="slide-sub-title ltn__secondary-color animated">Up To 50% Off Today Only!</h6>
-                                                    <h1 class="slide-title  animated">Gold Standard <br>Pre-Workout</h1>
-                                                    <div class="slide-brief animated d-none">
-                                                        <p>Predictive analytics is drastically changing the real estate industry. In the past, providing data for quick</p>
-                                                    </div>
-                                                    <h5 class="color-orange  animated">Starting at &16.99</h5>
-                                                    <div class="btn-wrapper  animated">
-                                                        <a href="shop.html" class="theme-btn-1 btn btn-effect-1">Shop now</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-5 col-md-5 col-sm-5 align-self-center">
-                                            <div class="slide-item-img">
-                                                <!-- <a href="shop.html"><img src="img/product/1.png" alt="Image"></a> -->
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- ltn__slide-item -->
-                        <div class="ltn__slide-item ltn__slide-item-10 section-bg-1 bg-image" data-bs-bg="{{asset('panel/img/slider/62.jpg')}}">
-                            <div class="ltn__slide-item-inner">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-lg-7 col-md-7 col-sm-7 align-self-center">
-                                            <div class="slide-item-info">
-                                                <div class="slide-item-info-inner ltn__slide-animation">
-                                                    <h4 class="slide-sub-title ltn__secondary-color animated text-uppercase">Welcome to our shop</h4>
-                                                    <h1 class="slide-title  animated">TGold Standard <br>Pre-Workout</h1>
-                                                    <div class="slide-brief animated d-none">
-                                                        <p>Predictive analytics is drastically changing the real estate industry. In the past, providing data for quick</p>
-                                                    </div>
-                                                    <div class="btn-wrapper  animated">
-                                                        <a href="shop.html" class="theme-btn-1 btn btn-effect-1 text-uppercase">Shop now</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-5 col-md-5 col-sm-5 align-self-center">
-                                            <div class="slide-item-img">
-                                                <!-- <a href="shop.html"><img src="{{asset('panel/img/slider/62.jpg')}}" alt="Image"></a> -->
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-5">
-                    <div class="ltn__banner-item">
-                        <div class="ltn__banner-img">
-                            <a href="shop.html"><img src="{{asset('panel/img/banner/17.jpg')}}" alt="Banner Image"></a>
-                        </div>
-                    </div>
-                    <div class="ltn__banner-item">
-                        <div class="ltn__banner-img">
-                            <a href="shop.html"><img src="{{asset('panel/img/banner/18.jpg')}}" alt="Banner Image"></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('home.partials.slider_area')
     <!-- SLIDER AREA END -->
 
     <!-- PRODUCT TAB AREA START (product-item-3) -->
@@ -88,52 +10,7 @@
     <!-- PRODUCT TAB AREA END -->
 
     <!-- FEATURE AREA START ( Feature - 3) -->
-    <div class="ltn__feature-area mt-35 mt--65---">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="ltn__feature-item-box-wrap ltn__feature-item-box-wrap-2 ltn__border section-bg-1">
-                        <div class="ltn__feature-item ltn__feature-item-8">
-                            <div class="ltn__feature-icon">
-                                <img src="{{asset('panel/img/icons/svg/8-trolley.svg')}}" alt="#">
-                            </div>
-                            <div class="ltn__feature-info">
-                                <h4>Free shipping</h4>
-                                <p>On all orders over $49.00</p>
-                            </div>
-                        </div>
-                        <div class="ltn__feature-item ltn__feature-item-8">
-                            <div class="ltn__feature-icon">
-                                <img src="{{asset('panel/img/icons/svg/9-money.svg')}}" alt="#">
-                            </div>
-                            <div class="ltn__feature-info">
-                                <h4>15 days returns</h4>
-                                <p>Moneyback guarantee</p>
-                            </div>
-                        </div>
-                        <div class="ltn__feature-item ltn__feature-item-8">
-                            <div class="ltn__feature-icon">
-                                <img src="{{asset('panel/img/icons/svg/10-credit-card.svg')}}" alt="#">
-                            </div>
-                            <div class="ltn__feature-info">
-                                <h4>Secure checkout</h4>
-                                <p>Protected by Paypal</p>
-                            </div>
-                        </div>
-                        <div class="ltn__feature-item ltn__feature-item-8">
-                            <div class="ltn__feature-icon">
-                                <img src="{{asset('panel/img/icons/svg/11-gift-card.svg')}}" alt="#">
-                            </div>
-                            <div class="ltn__feature-info">
-                                <h4>Offer & gift here</h4>
-                                <p>On all orders over</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('home.partials.features_area')
     <!-- FEATURE AREA END -->
 
     <!-- ABOUT US AREA START -->
---
+++ resources/views/home/partials/slider_area.blade.php
@@ -0,0 +1,79 @@
+<div class="ltn__slider-area ltn__slider-3---  section-bg-1--- mt-30">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7">
+                <div class="ltn__slide-active-2 slick-slide-arrow-1 slick-slide-dots-1 mb-30">
+                    <!-- ltn__slide-item -->
+                    <div class="ltn__slide-item ltn__slide-item-10 section-bg-1 bg-image" data-bs-bg="{{asset('panel/img/slider/71.jpg')}}">
+                        <div class="ltn__slide-item-inner">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-7 col-md-7 col-sm-7 align-self-center">
+                                        <div class="slide-item-info">
+                                            <div class="slide-item-info-inner ltn__slide-animation">
+                                                <h6 class="slide-sub-title ltn__secondary-color animated">Up To 50% Off Today Only!</h6>
+                                                <h1 class="slide-title  animated">Gold Standard <br>Pre-Workout</h1>
+                                                <div class="slide-brief animated d-none">
+                                                    <p>Predictive analytics is drastically changing the real estate industry. In the past, providing data for quick</p>
+                                                </div>
+                                                <h5 class="color-orange  animated">Starting at &16.99</h5>
+                                                <div class="btn-wrapper  animated">
+                                                    <a href="{{route('shop')}}" class="theme-btn-1 btn btn-effect-1">Shop now</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 align-self-center">
+                                        <div class="slide-item-img">
+                                            <!-- <a href="{{route('shop')}}"><img src="img/product/1.png" alt="Image"></a> -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- ltn__slide-item -->
+                    <div class="ltn__slide-item ltn__slide-item-10 section-bg-1 bg-image" data-bs-bg="{{asset('panel/img/slider/62.jpg')}}">
+                        <div class="ltn__slide-item-inner">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-7 col-md-7 col-sm-7 align-self-center">
+                                        <div class="slide-item-info">
+                                            <div class="slide-item-info-inner ltn__slide-animation">
+                                                <h4 class="slide-sub-title ltn__secondary-color animated text-uppercase">Welcome to our shop</h4>
+                                                <h1 class="slide-title  animated">TGold Standard <br>Pre-Workout</h1>
+                                                <div class="slide-brief animated d-none">
+                                                    <p>Predictive analytics is drastically changing the real estate industry. In the past, providing data for quick</p>
+                                                </div>
+                                                <div class="btn-wrapper  animated">
+                                                    <a href="{{route('shop')}}" class="theme-btn-1 btn btn-effect-1 text-uppercase">Shop now</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 align-self-center">
+                                        <div class="slide-item-img">
+                                            <!-- <a href="{{route('shop')}}"><img src="{{asset('panel/img/slider/62.jpg')}}" alt="Image"></a> -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <div class="ltn__banner-item">
+                    <div class="ltn__banner-img">
+                        <a href="{{route('shop')}}"><img src="{{asset('panel/img/banner/17.jpg')}}" alt="Banner Image"></a>
+                    </div>
+                </div>
+                <div class="ltn__banner-item">
+                    <div class="ltn__banner-img">
+                        <a href="{{route('shop')}}"><img src="{{asset('panel/img/banner/18.jpg')}}" alt="Banner Image"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
