9cd8201fb1b6eb32629b2e1bcf6f9af61ca13046
rashad14 <rashad.seyfullayev14@gmail.com>
2023-03-17 19:28:52 +0400

    profile changes

---
+++ resources/views/profile/partials/addresses.blade.php
@@ -0,0 +1,25 @@
+<div class="tab-pane fade" id="liton_tab_1_4">
+    <div class="ltn__myaccount-tab-content-inner">
+        <p>The following addresses will be used on the checkout page by default.</p>
+        <div class="row">
+            <div class="col-md-6 col-12 learts-mb-30">
+                <h4>Billing Address <small><a href="#">edit</a></small></h4>
+                <address>
+                    <p><strong>Alex Tuntuni</strong></p>
+                    <p>1355 Market St, Suite 900 <br>
+                        San Francisco, CA 94103</p>
+                    <p>Mobile: (123) 456-7890</p>
+                </address>
+            </div>
+            <div class="col-md-6 col-12 learts-mb-30">
+                <h4>Shipping Address <small><a href="#">edit</a></small></h4>
+                <address>
+                    <p><strong>Alex Tuntuni</strong></p>
+                    <p>1355 Market St, Suite 900 <br>
+                        San Francisco, CA 94103</p>
+                    <p>Mobile: (123) 456-7890</p>
+                </address>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/profile/partials/downloads.blade.php
@@ -0,0 +1,36 @@
+<div class="tab-pane fade" id="liton_tab_1_3">
+    <div class="ltn__myaccount-tab-content-inner">
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Product</th>
+                    <th>Date</th>
+                    <th>Expire</th>
+                    <th>Download</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Carsafe - Car Service PSD Template</td>
+                    <td>Nov 22, 2020</td>
+                    <td>Yes</td>
+                    <td><a href="#"><i class="far fa-arrow-to-bottom mr-1"></i> Download File</a></td>
+                </tr>
+                <tr>
+                    <td>Carsafe - Car Service HTML Template</td>
+                    <td>Nov 10, 2020</td>
+                    <td>Yes</td>
+                    <td><a href="#"><i class="far fa-arrow-to-bottom mr-1"></i> Download File</a></td>
+                </tr>
+                <tr>
+                    <td>Carsafe - Car Service WordPress Theme</td>
+                    <td>Nov 12, 2020</td>
+                    <td>Yes</td>
+                    <td><a href="#"><i class="far fa-arrow-to-bottom mr-1"></i> Download File</a></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
